c7660da45343ef618be66e590d75775499c5f0ee
Yuisei-Maruyama <yuisei.metro@gmail.com>
2021-07-27 20:30:24 +0900

    update: 遷移先の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
 import Header from './components/Header/index';
 import Maps from './components/Maps/index'
+import Art from './components/molecules/Art'
+import Cafe from './components/molecules/Cafe'
+import Food from './components/molecules/Food'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Maps></Maps>
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <Switch>
+          <Route exact path={'/'} component={() => <Maps />} />
+          <Route path={'/art'} component={() => <Art />} />
+          <Route path={'/cafe'} component={() => <Cafe />} />
+          <Route path={'/food'} component={() => <Food />}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
